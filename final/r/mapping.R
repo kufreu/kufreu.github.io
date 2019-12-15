fd48078e33bea85e1539a54dba0bc007057c2990
kufreu <55106351+kufreu@users.noreply.github.com>
2019-12-14 21:20:49 -0500

    Add files via upload

--- final/r/mapping.R
+++ final/r/mapping.R
@@ -20,9 +20,9 @@ library(viridisLite)
 
 #### loading data ####
 # data was in same folder as r project
-tractsMI <- st_read(dsn = "censusMI.gpkg", layer = "tracts")
-chicago <- st_read(dsn = "chicago.gpkg", layer = "tracts2010")
-chicagoCBD <- st_read(dsn = "chicago.gpkg", layer = "CBD")
+tractsMI <- st_read(dsn = "censusMI.gpkg", input = "tracts")
+chicago <- st_read(dsn = "chicago.gpkg", input = "tracts2010")
+chicagoCBD <- st_read(dsn = "chicago.gpkg", input = "CBD")
 
 
 #### getting counties & tracts ####
@@ -55,17 +55,17 @@ keweenaw <- countiesMI[countiesMI$fips == "083", ]
 
 
 #### distance/direction function ####
-distdir_from_point <- function (layer, center, prefix = "") {
-  if (missing(center)) {
+distdir_from_point <- function (input, origin, prefix = "") {
+  if (missing(origin)) {
     wgs84 <-
-      layer %>%
+      input %>%
       as("sf") %>%
       st_transform(3395) %>%
       st_geometry %>%
       st_centroid %>%
       st_transform(4326)
     cbd <-
-      layer %>%
+      input %>%
       as("sf") %>%
       st_transform(3395) %>%
       st_geometry %>%
@@ -78,7 +78,7 @@ distdir_from_point <- function (layer, center, prefix = "") {
       st_centroid %>%
       st_transform(4326)
     int <-
-      layer %>%
+      input %>%
       as("sf") %>%
       mutate(
         dist_unit = st_distance(wgs84, cbd),
@@ -87,14 +87,14 @@ distdir_from_point <- function (layer, center, prefix = "") {
       )
   } else {
     wgs84 <-
-      layer %>%
+      input %>%
       as("sf") %>%
       st_transform(3395) %>%
       st_geometry %>%
       st_centroid %>%
       st_transform(4326)
     cbd <-
-      center %>%
+      origin %>%
       as("sf") %>%
       st_transform(3395) %>%
       st_geometry %>%
@@ -106,7 +106,7 @@ distdir_from_point <- function (layer, center, prefix = "") {
       st_geometry %>%
       st_centroid %>%
       st_transform(4326)
-    int <- layer %>%
+    int <- input %>%
       as("sf") %>%
       mutate(
         dist_unit = st_distance(wgs84, cbd),
@@ -166,85 +166,107 @@ distdir_from_point <- function (layer, center, prefix = "") {
 
 #### maps with tracts ####
 #berrien
-distdir_from_point(center = berrien,
-                   layer = tractsMI,
-                   prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
-  filter(cbd_card_ord == "N") %>%
+distdir_from_point(origin = berrien,
+                   input = tractsMI) %>%
+  mutate("Distance (km)" = dist_double / 1000) %>%
+  filter(card_ord == "N") %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
-          color = "white") +
+  geom_sf(aes(fill = `Distance (km)`),
+          color = NA) +
   scale_fill_viridis() +
-  labs(title = "Census Tracts North of Berrien County")
+  labs(title = "Census Tracts North of Berrien County",
+       x = "Longitude",
+       y = "Latitude")
 
 
-distdir_from_point(center = berrien,
-                   layer = tractsMI,
+distdir_from_point(origin = berrien,
+                   input = tractsMI,
                    prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
-  filter(cbd_card_ord == "N" & `Distance in Kilometers` >= 400) %>%
+  mutate("Distance (km)" = cbd_dist_double / 1000) %>%
+  filter(cbd_card_ord == "N" & `Distance (km)` >= 400) %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
+  geom_sf(aes(fill = `Distance (km)`),
           color = "white") +
   scale_fill_viridis() +
   labs(title = "Census Tracts North of Berrien County and >= 400km")
 
 
-distdir_from_point(center = berrien,
-                   layer = tractsMI,
+distdir_from_point(origin = berrien,
+                   input = tractsMI,
                    prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
-  filter(cbd_card_ord == "NE" & `Distance in Kilometers` >= 200) %>%
+  mutate("Distance (km)" = cbd_dist_double / 1000) %>%
+  filter(cbd_card_ord == "NE" & `Distance (km)` >= 200) %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
+  geom_sf(aes(fill = `Distance (km)`),
           color = NA) +
   scale_fill_viridis() +
-  labs(title = "Census Tracts Northeast of Berrien County and >= 350km")
+  labs(title = "Census Tracts Northeast of Berrien County and >= 200km")
 
-distdir_from_point(center = berrien,
-                   layer = tractsMI,
+distdir_from_point(origin = berrien,
+                   input = tractsMI,
                    prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
-  filter(`Distance in Kilometers` >= 100 & `Distance in Kilometers` <= 200) %>%
+  mutate("Distance (km)" = cbd_dist_double / 1000) %>%
+  filter(`Distance (km)` >= 100 & `Distance (km)` <= 200) %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
+  geom_sf(aes(fill = `Distance (km)`),
           color = "white") +
   scale_fill_viridis() +
   labs(title = "Census Tracts Northeast of Berrien County and >= 350km")
 
-distdir_from_point(layer = tractsMI, center = berrien) %>%
+distdir_from_point(input = tractsMI, origin = berrien) %>%
   ggplot() +
   geom_sf(aes(fill = card_ord), color = "gray") +
-  scale_fill_discrete() +
+  scale_fill_discrete("viridis") +
   guides(fill = guide_legend(title = "Direction")) +
   labs(title = "Direction from Berrien County",
        subtitle = "distdir_from_point()")
 # washtenaw
-distdir_from_point(center = washtenaw,
-                   layer = tractsMI,
+distdir_from_point(origin = washtenaw,
+                   input = tractsMI,
                    prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
+  mutate("Distance (km)" = cbd_dist_double / 1000) %>%
   filter(cbd_card_ord == "NW") %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
+  geom_sf(aes(fill = `Distance (km)`),
           color = NA) +
   scale_fill_viridis() +
   labs(title = "Census Tracts Northwest of Washtenaw County")
 
 
-
-
-distdir_from_point(center = chicagoCBD,
-                   layer = chicago,
-                   prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
+# gogebic
+distdir_from_point(origin = gogebic,
+                   input = tractsMI,) %>%
+  mutate("Distance (km)" = dist_double / 1000) %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
-          color = "gray") +
+  geom_sf(aes(fill = `Distance (km)`),
+          color = NA) +
   scale_fill_viridis() +
-  labs(title = "Distance from the CBD of Chicago")
+  labs(title = "Distance of Census Tracts from Gogebic County",
+       x = "Longitude",
+       y = "Latitude")
 
-# counties
+# chicago
+distdir_from_point(origin = chicagoCBD,
+                   input = chicago,
+                   prefix = "cbd") %>%
+  mutate("Distance (km)" = cbd_dist_double / 1000) %>%
+  ggplot() +
+  geom_sf(aes(fill = `Distance (km)`),
+          color = NA) +
+  scale_fill_viridis() +
+  labs(title = "Distance of Tracts from the CBD of Chicago")
+
+distdir_from_point(origin = chicagoCBD,
+                   input = chicago,
+                   prefix = "") %>%
+  group_by(card_ord) %>%
+  mutate(dirmean = mean(MedHouseVa)) %>%
+  ggplot() +
+  geom_sf(aes(fill = dirmean), color = NA) +
+  scale_fill_viridis("Median House Value") +
+  labs(title = "Median House Value on the North Side of Chicago")
+
+#### maps with counties ####
 centroidBerrien <- berrien %>%
   st_transform(3395) %>%
   st_geometry %>%
@@ -253,14 +275,21 @@ centroidBerrien <- berrien %>%
   mutate(name = "Berrien County") 
 
 
-distdir_from_point(center = berrien,
-                   layer = countiesMI,
+distdir_from_point(origin = berrien,
+                   input = countiesMI,
                    prefix = "cbd") %>%
-  mutate("Distance in Kilometers" = cbd_dist_double / 1000) %>%
+  mutate("Distance (km)" = cbd_dist_double / 1000) %>%
   ggplot() +
-  geom_sf(aes(fill = `Distance in Kilometers`),
+  geom_sf(aes(fill = `Distance (km)`),
           color = "white") +
   scale_fill_viridis() +
   labs(title = "Distance from Berrien County")
 
+distdir_from_point(origin = berrien,
+                   input = countiesMI)%>%
+  ggplot() +
+  geom_sf(aes(fill = card_ord),
+          color = "white") +
+  scale_color_viridis() +
+  labs(title = "Distance from Berrien County")
 
