9166d7e46fba30f005a757f56f1ffdc877a498e2
kufreu <55106351+kufreu@users.noreply.github.com>
2020-05-25 16:55:59 -0400

    distdir using st_geod_azimuth from lwgeom

---
+++ final/r/distdir_lwgeom.R
@@ -0,0 +1,119 @@
+#### distdir ####
+
+# this function calculates distance in meters and direction in degrees from an origin (origin) to a destination (input)
+# this function is dependent on lwgeom, tidyverse (mostly dplyr), and sf
+# written by kufre u.
+
+distdir_lwgeom <- function (input, origin, prefix = "") {
+  library(lwgeom)
+  library(dplyr)
+  library(sf)
+  library(units)
+  
+  print("Distance is measured in meters.")
+  
+  input <- filter(input, st_is_empty(input) == F)
+  
+  wgs84 <-
+    input %>%
+    as("sf") %>%
+    st_transform(3395) %>%
+    st_geometry %>%
+    st_centroid %>%
+    st_transform(4326)
+  
+  if (missing(origin)) {
+    cbd <-
+      input %>%
+      as("sf") %>%
+      st_transform(3395) %>%
+      st_geometry %>%
+      st_centroid %>%
+      st_union %>%
+      st_centroid %>%
+      st_transform(4326)
+  } else {
+    cbd <-
+      origin %>%
+      as("sf") %>%
+      filter(st_is_empty(.) == F) %>%
+      st_transform(3395) %>%
+      st_geometry %>%
+      st_centroid %>%
+      st_union %>%
+      st_centroid %>%
+      st_transform(4326)
+  }
+  
+  int <-
+    input %>%
+    as("sf") %>%
+    mutate(distance = drop_units(st_distance(wgs84, cbd)),
+           o = cbd,
+           d = wgs84)
+  
+  dir <- function(x) {
+    st_geod_azimuth(st_sfc(x$o, x$d, crs = 4326))
+  }
+  
+  int$direction_degrees <- (drop_units(set_units(
+    set_units(apply(int, 1, dir), "radians"), "degrees"
+  )) + 360) %% 360
+  
+  result <- int %>%
+    select(!c("o", "d")) %>%
+    mutate(
+      direction_card_ord = ifelse(
+        direction_degrees <= 22.5 |
+          direction_degrees >= 337.5,
+        "N",
+        ifelse(
+          direction_degrees <= 67.5 &
+            direction_degrees >= 22.5,
+          "NE",
+          ifelse(
+            direction_degrees <= 122.5 &
+              direction_degrees >= 67.5,
+            "E",
+            ifelse(
+              direction_degrees <= 157.5 &
+                direction_degrees >= 112.5,
+              "SE",
+              ifelse(
+                direction_degrees <= 292.5 &
+                  direction_degrees >= 247.5,
+                "W",
+                ifelse(
+                  direction_degrees <= 247.5 &
+                    direction_degrees >= 202.5,
+                  "SW",
+                  ifelse(
+                    direction_degrees <= 337.5 &
+                      direction_degrees >= 292.5,
+                    "NW",
+                    ifelse(
+                      direction_degrees <= 202.5 &
+                        direction_degrees >= 157.5,
+                      "S",
+                      "nichts"
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  if (prefix == "") {
+    result
+  } else {
+    result <- result %>%
+      rename(
+        !!paste(prefix, "distance", sep = "_") := distance,
+        !!paste(prefix, "direction_degrees", sep = "_") := direction_degrees,
+        !!paste(prefix, "direction_card_ord", sep = "_") := direction_card_ord
+      )
+  }
+  return(result)
+}
