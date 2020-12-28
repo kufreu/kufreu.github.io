4638cb44ee89b0026c7fe10ec3ddc52c781aff11
Kufre <55106351+kufreu@users.noreply.github.com>
2020-12-27 22:27:40 -0500

    Update st_multipart_to_singleparts.R

--- musings/st_multipart_to_singleparts/st_multipart_to_singleparts.R
+++ musings/st_multipart_to_singleparts/st_multipart_to_singleparts.R
@@ -3,11 +3,8 @@ st_multipart_to_singleparts = function(x) {
   library(sf)
   
   mixed = x
-  
   mixed$id = 1:nrow(mixed)
-  
   geo_types = as.character(unique(st_geometry_type(mixed)))
-  
   str_geo = grepl("MULTI", geo_types)
   
   if (sum(str_geo) == 0) {
@@ -23,9 +20,7 @@ st_multipart_to_singleparts = function(x) {
   }
   
   multipart = mixed[st_geometry_type(mixed) == multi,]
-  
   id = multipart$id
-  
   rows = 1:nrow(multipart)
   
   singlepart = do.call(rbind, lapply(rows, function(s)
