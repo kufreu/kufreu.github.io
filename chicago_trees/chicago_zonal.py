1eea9d7afa1c12385c9a1e41a2e7a13f8365146b
Kufre <kufrenu@gmail.com>
2021-07-24 11:00:05 -0400

    minor changes to chicago_zonal.py

--- chicago_trees/chicago_zonal.py
+++ chicago_trees/chicago_zonal.py
@@ -11,32 +11,25 @@ from rasterstats import zonal_stats
 
 raster = 'cookcountylandcover2010\landcover_2010_cookcounty.img'
 
-# get raster resolution and crs
-with rio.open(raster) as src:
-    state_plane = src.crs
-
 # chicago tracts from chicago open data
-chicago = gpd.read_file('https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=GeoJSON').to_crs(state_plane)
+with rio.open(raster) as src:
+    chicago = gpd.read_file('https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=GeoJSON').to_crs(src.crs)
 
 # reading dbf file for raster categories
 dbf = gpd.read_file('cookcountylandcover2010\landcover_2010_cookcounty.img.vat.dbf')
 
-classification = dbf['Class']
-
 # cleaning up categories
-classification = [str(x).lower().replace('/','_').replace(' ','_') for x in classification]
-
-values =  dbf['Value'].tolist()
+classes = [str(x).lower().replace('/','_').replace(' ','_') for x in dbf['Class']]
 
 # creating dictionary to be used in zonal_stats()
-landcover = dict(zip(values,classification))
+land_cover = dict(zip(dbf['Value'].tolist(), classes))
 
-zonal = zonal_stats(chicago,raster,categorical = True,nodata = 0, category_map = landcover)
-
-zeros = [0] * len(chicago)
+zonal = zonal_stats(chicago.iloc[0:1],raster,categorical = True,nodata = 0, category_map = land_cover)
 
 # blank DataFrame to be filled in with values from zonal_stats()
-results = pd.DataFrame(dict((v,zeros) for k,v in landcover.items()))
+zeros = [0] * len(chicago)
+
+results = pd.DataFrame(dict((v,zeros) for k,v in land_cover.items()))
 
 results.insert(0,'geoid10', chicago['geoid10'])
 
@@ -51,6 +44,7 @@ chicago = chicago.merge(results, how = 'left', on = 'geoid10')
 # calculating canopy percentage for each tract
 chicago['pct_canopy'] = chicago['tree_canopy'] / chicago.area * 100
 
+# save results
 results.to_csv('chicago_land_cover.csv')
 
 chicago.to_file('chicago.gpkg', layer ='land_cover', driver="GPKG")
